b43c890d0c81a2a00b9162702d298882f95418a9
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-06-17 15:59:03 +0900

    Controller変更

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -2,8 +2,10 @@ package jp.co.sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administer;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -23,11 +25,21 @@ public class AdministratorController {
 	 * InsertAdministratorFormをインスタンス化
 	 * InsertAdministratorFormオブジェクトをModelオブジェクトに格納
 	 */
+	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
 	@RequestMapping("/toInsert")
 	public String toInsert() {
-		return "administer/insert";
+		return "administrator/insert";
+	}
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administer administer = new Administer();
+		administer.setName(form.getName());
+		administer.setMailAddress(form.getMailAddress());
+		administer.setPassword(form.getPassword());
+		administratorService.insert(administer);
+		return "redirect:/";
 	}
 }
